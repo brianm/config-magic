1a065aa46ed599d860b04d8667df5dd943da0dc6
Martin Traverso <mtraverso@acm.org>
2009-10-08 12:30:45 -0700

    removed commented code

--- src/test/java/org/skife/config/Config3.java
+++ src/test/java/org/skife/config/Config3.java
@@ -7,12 +7,4 @@ interface Config3
     public String getOption();
 
     public abstract String getOption2();
-//
-//    @Config("option3")
-//    public abstract String getOption3();
-//
-//    public String getOption4()
-//    {
-//        return "option4 default";
-//    }
 }
